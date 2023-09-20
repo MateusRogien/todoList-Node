3c968428efce7456e53bd8b1820e52757e96c840
MateusRogien <mat.rogien@gmail.com>
2023-09-20 16:50:32 -0400

    added deletion function

--- index.js
+++ index.js
@@ -3,20 +3,15 @@ const path = require("path");
 const routes = require("./routes/routes.js");
 const connectToDB = require("./database/db.js");
 
+connectToDB();
 const app = express();
 const port = 3000;
 
-connectToDB();
+
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded());
 app.use(routes);
-
-
-//Route
-app.get("/home", (req, res) => {
-  res.render("index");
-});
 
 app.listen(port, () =>
   console.log(`Server is running at http://localhost:${port}`)
